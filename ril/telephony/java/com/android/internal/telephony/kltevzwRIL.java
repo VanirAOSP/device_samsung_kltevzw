78d3ee3c05b552a3fa11a0b80dbd79742101a3e7
nuclearmistake <nuclearmistake@gmail.com>
2014-12-02 14:24:40 -0500

    Revert "Delay SIM status message until after radio is on"
    This reverts commit 259a581b93daef4fb6e96fd3225a713dcfde2eda.

--- ril/telephony/java/com/android/internal/telephony/kltevzwRIL.java
+++ ril/telephony/java/com/android/internal/telephony/kltevzwRIL.java
@@ -72,8 +72,6 @@ public class klteuscRIL extends RIL implements CommandsInterface {
     private boolean dialCode = needsOldRilFeature("newDialCode");
     private boolean samsungEmergency = needsOldRilFeature("samsungEMSReq");
 
-    private Message mPendingGetSimStatus;
-
     public klteuscRIL(Context context, int preferredNetworkType,
             int cdmaSubscription, Integer instanceId) {
         this(context, preferredNetworkType, cdmaSubscription);
@@ -901,28 +899,4 @@ public class klteuscRIL extends RIL implements CommandsInterface {
         send(rr);
     }
 
-    // Hack for Lollipop
-    // The system now queries for SIM status before radio on, resulting
-    // in getting an APPSTATE_DETECTED state. The RIL does not send an
-    // RIL_UNSOL_RESPONSE_SIM_STATUS_CHANGED message after the SIM is
-    // initialized, so delay the message until the radio is on.
-    @Override
-    public void
-    getIccCardStatus(Message result) {
-        if (mState != RadioState.RADIO_ON) {
-            mPendingGetSimStatus = result;
-        } else {
-            super.getIccCardStatus(result);
-        }
-    }
-
-    @Override
-    protected void switchToRadioState(RadioState newState) {
-        super.switchToRadioState(newState);
-
-        if (newState == RadioState.RADIO_ON && mPendingGetSimStatus != null) {
-            super.getIccCardStatus(mPendingGetSimStatus);
-            mPendingGetSimStatus = null;
-        }
-    }
 }
