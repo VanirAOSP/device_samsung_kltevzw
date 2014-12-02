94b30be78b88a3303728c4f97d70f008a32ad1d3
nuclearmistake <nuclearmistake@gmail.com>
2014-12-02 14:25:10 -0500

    Revert "Rebreand for vzw"
    This reverts commit c43db946cc2c7a846b38e24c3a6334b2a503b85d.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,7 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2587885568
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12442385920
 
 # RIL
-BOARD_RIL_CLASS := ../../../device/samsung/kltevzw/ril
+BOARD_RIL_CLASS := ../../../device/samsung/klteusc/ril
 
 # inherit from the proprietary version
 -include vendor/samsung/kltevzw/BoardConfigVendor.mk
